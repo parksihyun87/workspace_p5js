0d0ab07f90e2bc379683e80cd9851e7deebd2d10
parksihyun87 <solarhyun87@gmail.com>
2025-04-15 00:53:51 +0900

    2025-04-15 git init

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(100, 100);
+
+  background(200);
+
+  arc(50, 50, 80, 80, 0, PI + HALF_PI);
+
+  describe('A white circle on a gray canvas. The top-right quarter of the circle is missing.');
+}
